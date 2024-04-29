dcf30e26142661694a03a815dd8f8929975af0b5
briimiin <kipkorirbrimin005@gmail.com>
2024-04-29 09:10:39 +0300

    adds a callender

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const AddTransactionForm = ({ onSubmit }) => {
   const [date, setDate] = useState('');
@@ -18,39 +20,37 @@ const AddTransactionForm = ({ onSubmit }) => {
 
   return (
     <form onSubmit={handleSubmit} className='transaction-form'>
-     <div className='input'>
-      <input
-        type="text"
-        placeholder="Date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        className="form-input"
-
-      />
-      <input
-        type="text"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        className="form-input"
-      />
-      <input
-        type="text"
-        placeholder="Category"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        className="form-input"
-      />
-      <input
-        type="text"
-        placeholder="Amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-        className="form-input"
-      />
+      <div className='input'>
+        <DatePicker
+          selected={date}
+          onChange={(date) => setDate(date)}
+          dateFormat="yyyy-MM-dd"
+          placeholderText="Date"
+          className="form-input date-input"
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="form-input description-input"
+        />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="form-input"
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className="form-input"
+        />
       </div>
       <button type="submit" className='submit-button'>Add Transaction</button>
-
     </form>
   );
 };
